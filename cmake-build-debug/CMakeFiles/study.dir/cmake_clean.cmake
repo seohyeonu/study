ff0a01a450f324ddbf123c3ba214c81fbf86d8f9
seohyeonu <92859444+seohyeonu@users.noreply.github.com>
2021-12-20 18:30:10 +0900

    2021.12.20

---
+++ cmake-build-debug/CMakeFiles/study.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study.dir/main.c.obj"
+  "libstudy.dll.a"
+  "study.exe"
+  "study.exe.manifest"
+  "study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

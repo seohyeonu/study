3924b804ec146f1f36d303a320212f4b76bc263a
seohyeonu <92859444+seohyeonu@users.noreply.github.com>
2021-12-24 15:36:49 +0900

    12-24,15

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "stdlib.h"
 
 int main() {
-    printf("Hello\n");
-    printf("sed");
+    char arr[100] = {'\0'};
+    int inx=0;
+    gets(arr);
+
+    for(int i='a'; i<='z'; i++){
+        if(arr[inx] == i) printf("%d", i);
+        else printf("-1");
+    }
     return 0;
 }
